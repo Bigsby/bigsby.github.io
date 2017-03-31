a04355b8c28a5e4bc3549cd912e9583672f300d4
Bigsby <bigsby@live.com>
2017-03-31 06:20:44 +0100

    Dynamize

--- scripts/app/appBuilder.js
+++ scripts/app/appBuilder.js
@@ -1,16 +1,36 @@
 module.exports = {
     RegisterComponents: function (app) {
-        app.directive("url", function () {
-            return {
-                restrict: "E",
-                link: function link(scope, element, attrs) {
-                    element[0].outerHTML = "<a target='_blank' href='" + (attrs.href || attrs.link) + "'>" + (attrs.text || element.text() || attrs.link) + "</a>";
-                }
-            };
+
+
+        app.controller("mainController", ["data", function (data) {
+            this.sections = data.sections;
+        }]);
+
+        app.controller("developerController", ["data", function (data) {
+            angular.extend(this, data.development);
+        }]);
+
+        app.controller("workController", ["data", function (data) {
+            this.companies = data.work;
+        }]);
+
+        app.controller("petController", ["data", function (data) {
+            this.projects = data.pet;
+        }]);
+
+        app.component("devCategory", {
+            templateUrl: templatePath("devCategory"),
+            bindings: {
+                category: "<",
+                inner: "<"
+            }
         });
 
-        app.controller("theController", ["data", function(data){
-            angular.extend(this, data.all);
-        }]);
+        app.component("devItem", {
+            templateUrl: templatePath("devItem"),
+            bindings: {
+                item: "<"
+            }
+        });
     }
 }
